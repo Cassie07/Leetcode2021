08a51220d579c2eb16ccc877c53e1ee086291c2d
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2021-03-21 15:59:09 -0500

    Create 509. Fibonacci Number.py

---
+++ Recursive_or_DFS/509. Fibonacci Number.py
@@ -0,0 +1,12 @@
+class Solution:
+
+    def fib(self, n: int) -> int:
+    
+        #recursion
+        if n == 0:
+            return 0
+        
+        if n == 1:
+            return 1
+        
+        return self.fib(n-1) + self.fib(n-2)
