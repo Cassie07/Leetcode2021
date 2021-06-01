bff03e5f78dd97ad340c35ef414456f7d1b664c8
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2021-06-01 18:57:50 -0500

    Update 101. Symmetric Tree.py

--- Recursive/101. Symmetric Tree.py
+++ Recursive/101. Symmetric Tree.py
@@ -1,3 +1,4 @@
+# first version
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -19,3 +20,21 @@ class Solution:
             return False
         
         return self.isMirror(root1.left, root2.right) and self.isMirror(root1.right, root2.left) 
+
+ # second version   
+class Solution:
+def isSymmetric(self, root: TreeNode) -> bool:
+
+    def isMirror(root1, root2):
+        if root1 is None and root2 is None:
+            return True
+
+        if root1 is None or root2 is None:
+            return False
+
+        if root1.val != root2.val:
+            return False
+
+        return isMirror(root1.left, root2.right) and isMirror(root1.right, root2.left)
+
+    return isMirror(root,root)
