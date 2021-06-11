f63ff75faf8db461b5d8cd42c4f718c7c5b7df40
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2021-06-10 20:57:07 -0500

    Update 113. Path Sum II.py

--- Recursive/113. Path Sum II.py
+++ Recursive/113. Path Sum II.py
@@ -20,3 +20,20 @@ class Solution:
             
         
         return [[root.val] + i for i in ret]
+
+    
+   # version 2
+class Solution:
+    def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
+        
+
+        if not root:
+            return []
+
+        if not root.left and not root.right and targetSum -root.val == 0:
+            return [[root.val]]
+
+        re =  (self.pathSum(root.left, targetSum - root.val) + self.pathSum(root.right, targetSum - root.val))
+      
+        
+        return [[root.val] + i for i in re]
