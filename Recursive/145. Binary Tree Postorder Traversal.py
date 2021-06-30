06301c63d3ba80db23d8d5ec79b1ad49a8276fc5
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2021-06-29 20:48:21 -0500

    Update 145. Binary Tree Postorder Traversal.py

--- Recursive/145. Binary Tree Postorder Traversal.py
+++ Recursive/145. Binary Tree Postorder Traversal.py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 
-
+# Postorder: left --> right --> mid
 # Recursive
 class Solution:
     def postorderTraversal(self, root: TreeNode) -> List[int]:
@@ -29,3 +29,19 @@ class Solution:
         helper(root)
         
         return self.res
+    
+# Iterative
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        stack, res = [(root,False)], []
+        
+        while stack:
+            node, visit = stack.pop()
+            if node:
+                if not visit:
+                    stack.append((node, True))
+                    stack.append((node.right, False))
+                    stack.append((node.left, False))
+                else:
+                    res.append(node.val)
+        return res
