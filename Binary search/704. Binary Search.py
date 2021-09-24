794dfecbba40ee83c5a6ec9d1d93cd32eeb44cf9
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2021-09-24 14:52:03 -0500

    Create 704. Binary Search.py

---
+++ Binary search/704. Binary Search.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        def binary_search(low, high, arr, target):
+        
+            if low > high:
+                return -1
+            else:
+                mid = low + (high- low)//2
+
+                if target == nums[mid]:
+                    return mid
+                elif target < nums[mid]:
+                    return binary_search(low, mid-1, arr, target)
+                else:
+                    return binary_search(mid+1, high, arr, target)
+        return binary_search(0, len(nums) -1, nums, target)
