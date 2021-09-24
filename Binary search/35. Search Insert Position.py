f4bfb1053755a7ffd43f47b6d94b9829d7c315d1
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2021-09-24 13:18:30 -0500

    Create 35. Search Insert Position.py

---
+++ Binary search/35. Search Insert Position.py
@@ -0,0 +1,20 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+
+        
+        # Recursive
+        def binary_search(low, high, arr, target):
+            
+            
+            if low > high: return low
+
+            mid = low + (high-low)//2
+            
+            if arr[mid] == target:
+                return mid
+            elif arr[mid] < target:
+                return binary_search(mid + 1, high, arr, target)
+            else:
+                return binary_search(low, mid - 1, arr, target)
+        
+        return binary_search(0, len(nums)-1, nums, target)
