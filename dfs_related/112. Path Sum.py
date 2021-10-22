259669bc546aebcdb5afac84693234785bb0439b
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2021-10-21 20:50:30 -0500

    Delete dfs_related directory

--- dfs_related/112. Path Sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
-        
-        if not root: return False
-        
-        if not root.left and not root.right and root.val == targetSum:
-            return True
-        
-        return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum - root.val)
