532e67d039945e79f2b605457fa62ce9949903a1
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2021-11-04 23:10:25 -0500

    Update 49. Group Anagrams.py

--- Hash_table/49. Group Anagrams.py
+++ Hash_table/49. Group Anagrams.py
@@ -1,7 +1,21 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
+        # solution 1: sorted(str)
+        dic_word = {}
+        for word in strs:
+            
+            word_list = ''
+            word_list = tuple(sorted(word))
+            
+            if word_list in dic_word.keys():
+                dic_word[word_list].append(word)
+            else:
+                dic_word[word_list] = [word]
         
+        return [i for i in dic_word.values()]
+    
+        # solution2: list.sort()
         string = 'abcdefghijklmnnopqrstuvwxyz'
         dictionary = {letter:index+1 for index,letter in enumerate(string)}
         
