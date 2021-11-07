e8c094c188f8c069b1026f9b24d7c7821d9ba4b8
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2021-11-06 20:49:38 -0500

    Create 252. Meeting Rooms.py

---
+++ Sorting_related/252. Meeting Rooms.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        
+        # Timsort : O(nlogn)
+        intervals.sort()
+        print(intervals)
+        
+        # O(n)
+        for index in range(len(intervals)-1):
+            
+            interval1 = intervals[index]
+            interval2 = intervals[index + 1]
+            
+            if interval1[1] > interval2[0]:
+                return False
+            
+        # So the entire time complexity should O(nlogn + n) = O(nlogn)
+        
+        return True
