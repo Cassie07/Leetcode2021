fc0da2e78080f3b3c1125d1073e2753262657abd
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2021-11-06 20:45:50 -0500

    heap

---
+++ Intuit_Karat/253. Meeting Rooms II.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        
+        intervals.sort()
+        
+        heap = []
+        
+        for i in intervals:
+            
+            if heap and i[0] >= heap[0]:
+                heapreplace(heap, i[1])
+            else:
+                heappush(heap, i[1])
+        
+        return len(heap)
+        
