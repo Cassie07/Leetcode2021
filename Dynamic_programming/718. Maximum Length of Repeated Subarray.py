d2a49753bb5c08f83b1ef6eebdd6ff5fd45aec0c
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2021-11-09 13:32:28 -0600

    Update 718. Maximum Length of Repeated Subarray.py

--- Dynamic_programming/718. Maximum Length of Repeated Subarray.py
+++ Dynamic_programming/718. Maximum Length of Repeated Subarray.py
@@ -1,3 +1,20 @@
+# bottom-up: better solution
+class Solution:
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        
+        
+        dp =[[0] * (len(nums2) + 1) for i in range(len(nums1) + 1)]
+        
+
+            
+        for i in reversed(range(len(nums1))):
+            for j in reversed(range(len(nums2))):
+                if nums1[i] == nums2[j]:
+                    dp[i][j] = dp[i+1][j+1] + 1
+
+        return max(max(item) for item in dp)
+
+# top-bottom
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
         
