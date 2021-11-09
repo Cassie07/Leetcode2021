58260c4402947830cddea87d37e5286e4aadc1c9
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2021-11-09 13:25:51 -0600

    Create 1143. Longest Common Subsequence.py

---
+++ Dynamic_programming/1143. Longest Common Subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        
+        dp = [[0] * (len(text2) + 1) for i in range(len(text1) + 1)]
+
+        for i in reversed(range(len(text1))):
+            
+            for j in reversed(range(len(text2))):
+                
+                if text1[i] == text2[j]:
+                    dp[i][j] = dp[i+1][j+1] + 1
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+        
+        return max([max(i) for i in dp])
+                
