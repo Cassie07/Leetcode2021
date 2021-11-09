1786af7c65447e1129bef94db6c5b30d96ea357b
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2021-11-09 13:25:05 -0600

    Update 1143. Longest Common Subsequence.py

--- Intuit_Karat/1143. Longest Common Subsequence.py
+++ Intuit_Karat/1143. Longest Common Subsequence.py
@@ -3,13 +3,14 @@ class Solution:
         
         dp = [[0] * (len(text2) + 1) for i in range(len(text1) + 1)]
 
-        for i in range(1, len(text1) + 1):
+        for i in reversed(range(len(text1))):
             
-            for j in range(1, len(text2) + 1):
+            for j in reversed(range(len(text2))):
                 
-                if text1[i-1] == text2[j-1]:
-                    dp[i][j] = dp[i-1][j-1] + 1
+                if text1[i] == text2[j]:
+                    dp[i][j] = dp[i+1][j+1] + 1
                 else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
         
         return max([max(i) for i in dp])
+                
