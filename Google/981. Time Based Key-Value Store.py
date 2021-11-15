fa386bb6f2b6c6f9f88ad7d6e18144b19cee3518
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2021-11-14 21:58:20 -0600

    Update 981. Time Based Key-Value Store.py

--- Google/981. Time Based Key-Value Store.py
+++ Google/981. Time Based Key-Value Store.py
@@ -12,16 +12,47 @@ class TimeMap:
 
     def get(self, key: str, timestamp: int) -> str:
         if key in self.dic.keys():
-            l,r = 0, len(self.dic[key])
-            while l < r:
+            l,r = 0, len(self.dic[key]) -1
+            while l <= r:
 
                 mid = l + (r-l) // 2
+                
+                if timestamp == self.dic[key][mid][1]:
+                    return self.dic[key][mid][0]
 
                 if timestamp < self.dic[key][mid][1]:
-                    r = mid
+                    r = mid - 1
                 else:
                     l = mid + 1
 
-            if r == 0: return ''
-            else: return self.dic[key][r-1][0]
+            if r == -1: return ''
+            else: return self.dic[key][r][0]
         return ''
+
+# class TimeMap:
+
+#     def __init__(self):
+#         self.dic = {}
+
+#     def set(self, key: str, value: str, timestamp: int) -> None:
+#         if key not in self.dic.keys():
+#             self.dic[key] = [(value, timestamp)]
+#         else:
+#             self.dic[key].append((value,timestamp))
+        
+
+#     def get(self, key: str, timestamp: int) -> str:
+#         if key in self.dic.keys():
+#             l,r = 0, len(self.dic[key])
+#             while l < r:
+
+#                 mid = l + (r-l) // 2
+
+#                 if timestamp < self.dic[key][mid][1]:
+#                     r = mid
+#                 else:
+#                     l = mid + 1
+
+#             if r == 0: return ''
+#             else: return self.dic[key][r-1][0]
+#         return ''
