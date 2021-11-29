f25d589b6730ae118c5f26908fdc4149beb77570
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2021-11-28 21:18:57 -0600

    Update 112. Path Sum.py

--- Tree_DFS_BFS_BST/112. Path Sum.py
+++ Tree_DFS_BFS_BST/112. Path Sum.py
@@ -7,3 +7,19 @@ class Solution:
             return True
         
         return self.hasPathSum(root.left, targetSum - root.val) or self.hasPathSum(root.right, targetSum - root.val)
+
+# much faster one: 28s
+class Solution:
+    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
+        
+        def helper(root, target):
+            if root:
+                
+                if not root.left and not root.right and root.val == target:
+                    return True
+                
+                return helper(root.left, target - root.val) or helper (root.right, target - root.val)
+            else:
+                return False
+            
+        return helper(root, targetSum)
