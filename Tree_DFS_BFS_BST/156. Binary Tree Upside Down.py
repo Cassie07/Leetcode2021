e2c84324fb16bf149e05428a7584e8499145e7da
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2021-12-06 22:19:46 -0600

    Create 156. Binary Tree Upside Down.py

---
+++ Tree_DFS_BFS_BST/156. Binary Tree Upside Down.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def upsideDownBinaryTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        def dfs(root):
+            if not root or (not root.left and not root.right):
+                return root
+            
+            left = dfs(root.left)
+            root.left.left = root.right
+            root.left.right = root
+            
+            root.left = None
+            root.right = None
+            
+            return left
+        
+        return dfs(root)
+        
