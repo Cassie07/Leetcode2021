209c7e056778a620f76c126d2e0d8033e3a38f22
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2021-12-09 23:43:37 -0600

    Update 735. Asteroid Collision.py

--- Google/735. Asteroid Collision.py
+++ Google/735. Asteroid Collision.py
@@ -1,18 +1,20 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         
-        
         ans = []
         
         for new in asteroids:
             while ans and new < 0 < ans[-1]:
+                # ruin ans[-1], continue comparison
                 if ans[-1] < -new:
                     ans.pop()
-                    continue
+                # ruin both ans[-1] and new, pop ans[-1] and do not need to as new into ans, stop the comparison
                 elif ans[-1] == -new:
                     ans.pop()
-                
-                break
+                    break
+                # ruin new, stop the comparison
+                else:
+                    break
             else:
                 ans.append(new)
         return ans
