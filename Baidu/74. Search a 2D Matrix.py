b04ecd884899971749aa8684a69ba2f03afe6c6d
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2022-01-04 11:12:46 -0600

    Create 74. Search a 2D Matrix.py

---
+++ Baidu/74. Search a 2D Matrix.py
@@ -0,0 +1,41 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        def binary_search(l,h):
+            
+            if l < h:
+                
+                mid = l + (h - l) //2
+                
+                if target in matrix[mid]:
+                    return True
+                
+                if target < matrix[mid][0]:
+                    return binary_search(l,mid)
+                elif target > matrix[mid][-1]:
+                    return binary_search(mid + 1,h)
+      
+            else:
+                return False
+            
+        return binary_search(0, len(matrix))class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        def binary_search(l,h):
+            
+            if l < h:
+                
+                mid = l + (h - l) //2
+                
+                if target in matrix[mid]:
+                    return True
+                
+                if target < matrix[mid][0]:
+                    return binary_search(l,mid)
+                elif target > matrix[mid][-1]:
+                    return binary_search(mid + 1,h)
+      
+            else:
+                return False
+            
+        return binary_search(0, len(matrix))
