282e4c8e160e619c97cf14fea6908d4446119d45
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2022-01-19 10:19:08 -0600

    Create 696. Count Binary Substrings696. Count Binary Substrings.py

---
+++ Amazon/696. Count Binary Substrings696. Count Binary Substrings.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countBinarySubstrings(self, s: str) -> int:
+        
+        
+        cur = s[0]
+        res = []
+        tmp = 1
+        for i in s[1:]:
+            if i == cur:
+                tmp += 1
+            else:
+                res.append(tmp)
+                tmp = 1
+                cur = i
+        res.append(tmp)
+        
+        r = 0
+        for i in range(len(res)-1):
+            r += min(res[i], res[i+1])
+            
+        return r
+                
